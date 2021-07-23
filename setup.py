782c4e43f057094ac246077b7f8a819ea9f0abc9
Daniele Sirocchi <daniele.sirocchi@xonne.it>
2021-07-23 15:28:16 +0200

    Adde setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nn_utils',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/dsr-lab/simple_nn',
+    license='',
+    author='dsirocchi',
+    author_email='',
+    description=''
+)
