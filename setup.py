13a448fc3200864b92b549f02520285b9b279782
Daniele Sirocchi <daniele.sirocchi@xonne.it>
2021-07-23 15:46:07 +0200

    Minox fix to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nn_utils',
     version='1.0',
-    packages=[''],
+    packages=['utils'],
     url='https://github.com/dsr-lab/simple_nn',
     license='',
     author='dsirocchi',
